b405cc6071c56b686c6f5462e49d914fde46eef3
trung.do <trung.do@flynk.com>
2017-11-10 19:23:17 +0700

    add Cors parameter

--- myapp/app/main.js
+++ myapp/app/main.js
@@ -1,6 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+let url = "http://localhost:8080/pce/app_form_api/rest?cors=true";
+fetch(url)
+.then(response => {
+    console.log("response: ", response)
+    return response.json();
+});
+
 var Content = React.createClass({
     render: function() {
         return (
