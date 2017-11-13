e365c7b7b60a14bd7f489ec971fd175bd6886f8d
trung.do <trung.do@flynk.com>
2017-11-13 15:13:12 +0700

    using React 16

--- myapp/app/main.js
+++ myapp/app/main.js
@@ -1,21 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+// import 'whatwg-fetch';
 
-let url = "http://localhost:8080/pce/app_form_api/rest?cors=true";
-fetch(url)
-.then(response => {
-    console.log("response: ", response)
-    return response.json();
+let data = JSON.stringify('trung');
+let url = "http://localhost:8080/pce/app_form_api/post";
+// fetch(url)
+let myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+myHeaders.append("Content-Type", "application/json");
+
+
+fetch(url,  {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+        'Cors': 'true'
+      },
+    data
+})
+.then(res => res.json())
+.then(json => {
+    console.log("response: ", json)
 });
 
-var Content = React.createClass({
-    render: function() {
+class Content extends React.Component {
+    render() {
         return (
         <div>
-        	<p> It's me 3</p>
+        	<p> It's me - TRUNG</p>
             <b>Congratulations</b>, you are now ready to implement your client side application! Enjoy :-)
         </div>
         );
     }
-});
+}
 ReactDOM.render(<Content />, document.getElementById('content'));
